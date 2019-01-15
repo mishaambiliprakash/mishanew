345c6c51bea2a5d15e0934a1908dca075b1ae063
mishaambiliprakash <ab224105@gmail.com>
2019-01-15 16:04:32 +0530

    calculater created

--- app/src/main/java/com/mish/administrator/myapplication/Calculater.java
+++ app/src/main/java/com/mish/administrator/myapplication/Calculater.java
@@ -2,12 +2,34 @@ package com.mish.administrator.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Calculater extends AppCompatActivity {
-
+EditText ed1,ed2;
+    Button b;
+    String s1,s2,s3;
+    Integer number1,number2,number3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculater);
+        ed1=(EditText)findViewById(R.id.num1);
+        ed2=(EditText)findViewById(R.id.num2);
+        b=(Button)findViewById(R.id.buttonadd);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               s1=ed1.getText().toString();
+                s2=ed2.getText().toString();
+                number1=Integer.parseInt(s1);
+                number2=Integer.parseInt(s2);
+                number3=number1+number2;
+                s3=String.valueOf(number3);
+                Toast.makeText(getApplicationContext(),s3,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
